f0cf75ef6864dbdc57660b5d954293192591f3bb
Ryoichi Nakajo <ryoichi-nakajo@aist.go.jp>
2025-02-09 09:14:21 +0900

    [lstm] update a training script

--- scripts/aligning/lstm_benchmark.sh
+++ scripts/aligning/lstm_benchmark.sh
@@ -1,8 +1,6 @@
-#   --multirun seed=0,1,2,3,4,5 \
 python run.py --config-name=aligning_config \
-              agents=lstm_agent \
-              agent_name=lstm \
-              window_size=3 \
-              group=aligning_lstm_seeds \
-              epoch=100 \
-              eval_every_n_epochs=10
+    --multirun seed=0,1,2,3,4,5 \
+    agents=lstm_agent \
+    agent_name=lstm \
+    window_size=3 \
+    group=aligning_lstm_seeds
